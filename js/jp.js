27f89bb762194010e8eeca47f05cf8ea1aef1bea
jig101 <mrjp123@googlemail.com>
2015-07-19 23:41:02 +0100

    Version 1

---
+++ js/jp.js
@@ -0,0 +1,12 @@
+/**
+ * Jigs edits 
+ *
+ * 
+ */
+
+( function( $ ) {
+	$( document ).ready(function() {
+  		$('ul#top-menu-id li').addClass('pure-u-2-24'); 
+		$('.pure-menu-list li').addClass('pure-menu-item');$('.pure-menu-list li a').addClass('pure-menu-link');
+	});
+} )( jQuery );
